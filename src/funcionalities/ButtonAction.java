84fc24d85242563885acca217d312980ecdb7ee9
mysteryblocks <ricnogueira01@outlook.com>
2017-12-18 23:48:20 +0000

    adicionar o calculo de fp e fns

--- src/funcionalities/ButtonAction.java
+++ src/funcionalities/ButtonAction.java
@@ -17,6 +17,8 @@ import javax.swing.table.DefaultTableModel;
 
 import org.omg.CORBA.portable.ValueBase;
 
+import antiSpamFilter.AntiSpamFilterAutomaticConfiguration;
+
 public class ButtonAction {
 
 	private PrintWriter writer;
@@ -26,112 +28,184 @@ public class ButtonAction {
 	private int counterFP = 0;
 	private JTable manualTable;
 	private JTable automaticTable;
-	
+	private File spam;
+	private File ham;
+
 	public void searchFile(JTextField searchDirectory) {
-		
+
 		this.searchDirectory = searchDirectory;
-		
+
 		File f = new File("./" + searchDirectory.getName());
-	
+
 		if(!f.exists()){
-			
+
 			JFileChooser fileChooser = new JFileChooser();
-	 
-	        // For File
-	        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-	        
-	        fileChooser.setAcceptAllFileFilterUsed(false);
-	 
-	        int rVal = fileChooser.showOpenDialog(null);
-	        
-	        //Escolher o ficheiro correcto
-	        if (rVal == JFileChooser.APPROVE_OPTION) {
-	        	
-	        	if(searchDirectory.getName().equals("rules.cf")){
-	        		if(fileChooser.getSelectedFile().getName().equals("rules.cf")){
-	        			searchDirectory.setText(fileChooser.getSelectedFile().toString());
-	        		}
-	        		else{
-	        			JOptionPane.showMessageDialog(null, "Não escolheste o ficheiro rules.cf");
-	        		}
-	        	}
-	        	if(searchDirectory.getName().equals("spam.log")){
-	        		if(fileChooser.getSelectedFile().getName().equals("spam.log")){
-	        			searchDirectory.setText(fileChooser.getSelectedFile().toString());
-	        		}
-	        		else{
-	        			JOptionPane.showMessageDialog(null, "Não escolheste o ficheiro spam.log");
-	        		}
-	        	}
-	        	if(searchDirectory.getName().equals("ham.log")){
-	        		if(fileChooser.getSelectedFile().getName().equals("ham.log")){
-	        			searchDirectory.setText(fileChooser.getSelectedFile().toString());	
-	        			
-	        		}
-	        		else{
-	        			JOptionPane.showMessageDialog(null, "Não escolheste o ficheiro ham.log");
-	        		}
-	        	}
-	        }
-	        
-	        lf = new LeitorDeFicheiro();
-	        try{
-	        	lf.createTables(searchDirectory);   
-	        }catch (Exception e) {
+
+			// For File
+			fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+			fileChooser.setAcceptAllFileFilterUsed(false);
+
+			int rVal = fileChooser.showOpenDialog(null);
+
+			//Escolher o ficheiro correcto
+			if (rVal == JFileChooser.APPROVE_OPTION) {
+
+				if(searchDirectory.getName().equals("rules.cf")){
+					if(fileChooser.getSelectedFile().getName().equals("rules.cf")){
+						searchDirectory.setText(fileChooser.getSelectedFile().toString());
+					}
+					else{
+						JOptionPane.showMessageDialog(null, "Não escolheste o ficheiro rules.cf");
+					}
+				}
+				if(searchDirectory.getName().equals("spam.log")){
+					if(fileChooser.getSelectedFile().getName().equals("spam.log")){
+						searchDirectory.setText(fileChooser.getSelectedFile().toString());
+					}
+					else{
+						JOptionPane.showMessageDialog(null, "Não escolheste o ficheiro spam.log");
+					}
+				}
+				if(searchDirectory.getName().equals("ham.log")){
+					if(fileChooser.getSelectedFile().getName().equals("ham.log")){
+						searchDirectory.setText(fileChooser.getSelectedFile().toString());	
+
+					}
+					else{
+						JOptionPane.showMessageDialog(null, "Não escolheste o ficheiro ham.log");
+					}
+				}
+			}
+
+			lf = new LeitorDeFicheiro();
+			try{
+				lf.createTables(searchDirectory);   
+			}catch (Exception e) {
 				System.out.println("");
 			}
 		}
-		
+
 		else{
 			searchDirectory.setText("./" + f.getName());
-	        lf = new LeitorDeFicheiro();
-	        if(searchDirectory.getName().equals("rules.cf"))
-	        	lf.createTables(searchDirectory); 
+			if(searchDirectory.getName().equals("spam.log"))
+				spam = new File("./spam.log");
+			if(searchDirectory.getName().equals("ham.log"))
+				ham = new File("./ham.log");
+			lf = new LeitorDeFicheiro();
+			if(searchDirectory.getName().equals("rules.cf"))
+				lf.createTables(searchDirectory); 
 		}
 	}
-	
+
 	public void save(JTextField fileDirectory, JTable table) {
-				
-        File f = new File(fileDirectory.getText());
-        
-        Scanner sc = null;
-        FileWriter fw = null;
-  
-        try {
+
+		File f = new File(fileDirectory.getText());
+
+		Scanner sc = null;
+		FileWriter fw = null;
+
+		try {
 			sc = new Scanner(f);
 			fw = new FileWriter(f);
-				
+
 			for(int j = 0 ; j < table.getRowCount(); j++){
 				fw.write(table.getValueAt(j,0) + " " + table.getValueAt(j, 1) + "\n");
 			}
-			
+
 		} catch (Exception e) {
 
 		}
 
-        try {
-        	sc.close();
+		try {
+			sc.close();
 			fw.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void gerar(JTable table, String spamDirectory, String hamDirectory, JButton button){
 
+		counterFN = 0;
+		counterFP = 0;
 		
-	}
-	
-	public void calculateFP(JTable table){
-		
-		
-	}
-	
-	public void calculateFN(JTable table){
-		
+		if(button.getText().equals("Avaliar Qualidade")){
+			table = manualTable;
+			
+			lf.lerFicheiroLog(spamDirectory);
+
+			calculateFP(table);
+
+			lf.lerFicheiroLog(hamDirectory);
+
+			calculateFN(table);
+		}
+
+		if(button.getText().equals("Gerar")){
+			AntiSpamFilterAutomaticConfiguration antiSpamFilter = new AntiSpamFilterAutomaticConfiguration();
+			try {
+				antiSpamFilter.init();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			table = automaticTable;
+		}
 
 	}
-	
+
+	public void calculateFP(JTable table){
+		double contaSpam = 0.0;
+
+		for(int i = 0; i < lf.getLog().size(); i++){
+
+			if(!lf.getLog().get(i).equals("0.0")){
+				String rule = lf.getLog().get(i);
+
+				for(int j = 0; j < table.getRowCount(); j++){
+					if(rule.equals(table.getValueAt(j, 0))){
+						contaSpam += (double) table.getValueAt(j, 1);
+					}
+				}
+			}
+
+			else{
+				if(contaSpam < 0.0)
+					counterFP++;
+
+				contaSpam = 0.0;
+			}
+		}
+
+
+
+	}
+
+	public void calculateFN(JTable table){
+
+		double contaHam = 0.0;
+
+		for(int i = 0; i < lf.getLog().size(); i++){
+
+			if(!lf.getLog().get(i).equals("0.0")){
+				String rule = lf.getLog().get(i);
+
+				for(int j = 0; j < table.getRowCount(); j++){
+					if(rule.equals(table.getValueAt(j, 0))){
+						contaHam += (double) table.getValueAt(j, 1);
+					}
+				}
+			}
+
+			else{
+				if(contaHam > 0.0)
+					counterFN++;
+
+				contaHam = 0.0;
+			}
+		}
+	}
+
 	public JTextField getSearchDirectory(){
 		return searchDirectory;
 	}
@@ -139,30 +213,37 @@ public class ButtonAction {
 	public void setManualTable(JTable table){
 		manualTable = table;
 	}
-	
+
 	public void setAutomaticTable(JTable table){
 		automaticTable = table;
 	}
-	
+
 	public JTable getManualTable(){
 		if(manualTable == null)
 			return lf.getManualTable();
-	
+
 		return manualTable;
 	}
-	
+
 	public JTable getAutomaticTable(){
 		if(automaticTable == null)
 			return lf.getAutomaticTable();
-		
+
 		return automaticTable;
 	}
-	
+
 	public int getCounterFN(){
 		return counterFN;
 	}
-	
+
 	public int getCounterFP(){
 		return counterFP;
+	}
+	
+	public File getSpam() {
+		return spam;
+	}
+	public File getHam() {
+		return ham;
 	}
 }
